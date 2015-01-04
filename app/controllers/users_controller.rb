7b6e5217edd2d643e2094030506d20bfbe4ee002
Leeingnyo <dyd1928@naver.com>
2015-01-04 22:36:03 +0900

    user show
    if the user has a token,
    the server render user's information
     * id
     * student_number
     * subjects
       * id
    	 * name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,20 @@ require 'securerandom'
 require 'digest'
 
 class UsersController < ApplicationController
+	def show
+		token = params[:token]
+		user = User.find params[:id]
+		if user and user.token == token
+			subjects = Array.new
+			for subject in user.subjects
+				subjects << { 'id' => subject.id, 'subject_name' => subject.subject_name }
+			end
+			render json: { id: user.id, student_number: user.student_number, subjects: subjects }
+		else
+			render json: { result: 'fail' }
+		end
+	end
+
 	def create
 		user = User.new
 		user.student_number = params[:student_number]
