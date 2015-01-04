9269b85cb262b9fc25b1ad817d7f1deb39fca9bd
Leeingnyo <dyd1928@naver.com>
2015-01-05 01:17:22 +0900

    make function search to user model
    'keyword' is splited each syllable and transforms to '%k%e%y%w%o%r%d%'
    and use this to subject db query (?)

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,14 @@
 class Subject < ActiveRecord::Base
 	has_and_belongs_to_many :users
+
+	def self.search(keyword)
+		query = ''
+		for c in keyword.split(//u) do query << '%' + c end
+		query << '%'
+		if keyword
+			where('subject_name LIKE ?', query)
+		else
+			all
+		end
+	end
 end
