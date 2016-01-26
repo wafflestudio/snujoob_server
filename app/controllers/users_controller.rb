5460caa568a4f95f2a170ac7fd4e34d90c71b022
Leeingnyo <dyd1928@naver.com>
2016-01-27 05:44:16 +0900

    implement login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
+  
+  def login
+    user = User.find_by student_id: params[:student_id]
+    if user and user.check_password params[:password]
+      user.generate_token
+      user.save
+      render json: {
+        'result': true,
+        'token': user.login_token,
+      }
+    else
+      render json: {
+        'result': false,
+      }
+    end
+  end
 end
