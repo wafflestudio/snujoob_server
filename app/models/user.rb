aab3f9f8d39c7059af8dd26fe40d08e9b766d8b4
Leeingnyo <dyd1928@naver.com>
2016-01-27 04:59:38 +0900

    make password function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
+require 'digest'
+require 'securerandom'
+
 class User < ActiveRecord::Base
   has_many :watchings
   has_many :lectures, through: :watchings
+
+  validates :student_id, presence: true, uniqueness: true, format: { with: /\A20[0-9]{2}-[12][0-9]{4}\z/ }
+  validates :password, presence: true
+  validates :salt, presence: true
+
+  def set_password(password)
+    self.salt = SecureRandom.hex
+    self.password = Digest::SHA256.hexdigest(password + self.salt)
+  end
+
+  def check_password(password)
+    self.password == Digest::SHA256.hexdigest(password + self.salt)
+  end
 end
