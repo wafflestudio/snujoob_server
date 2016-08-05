b484074276ef2c61649284093ad472060775f10c
Leeingnyo <dyd1928@naver.com>
2016-08-05 16:26:20 +0900

    fix websocket problem
    version go up -> redis error

--- app/assets/javascripts/components/notification.js.jsx
+++ app/assets/javascripts/components/notification.js.jsx
@@ -1,25 +1,25 @@
 var Notification = React.createClass({
   push: function(id){
     if (this.props.isRegistered(id)){
-      var link = '<a href="http://sugang.snu.ac.kr/sugang/co/co012.action">수강신청 페이지로 가기</a>'
-      var sound = '<audio loop autoplay><source src="http://leeingnyo.me/repository/44f0f77e92596c0707bea55d1a079793cac50a28faa2b951c5adca0e37b0cb3e/wu.mp3" type="audio/mpeg"></audio>'
+      var link = <a href="http://sugang.snu.ac.kr/sugang/co/co012.action">수강신청 페이지로 가기</a>
+      var sound = <audio loop autoPlay><source src="http://leeingnyo.me:8080/repository/44f0f77e92596c0707bea55d1a079793cac50a28faa2b951c5adca0e37b0cb3e/%EA%B8%B0%EC%83%81%EB%82%98%ED%8C%94.mp3" type="audio/mpeg" /></audio>
       var a = setInterval(function(){alert('자리가 비었습니다!'); clearInterval(a)}, 1 * 1000)
-      this.setState({message: '자리가 비었습니다! ' + link + sound})
+      this.setState({message: <span> <span>자리가 비었습니다!</span> {link} {sound} </span>})
     }
   },
   getInitialState: function(){
     return {
       pushed: false,
-      message: '',
+      message: null,
     };
   },
   componentDidMount: function(){
     console.log('bind')
+    console.log(this.props.chan)
     this.props.chan.bind('push', function(data){
-      console.log('asdf')
       this.push(data.lecture_id)
-      alert('asdf')
     }.bind(this))
+    console.log(this.props.chan)
   },
   render: function(){
     return (
